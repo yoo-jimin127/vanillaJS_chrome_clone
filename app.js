b0c006b8bd6ee0468a8f34c4e686f16c1281f3bc
yoo-jimin127 <dbwlals9936@naver.com>
2022-02-02 15:56:42 +0900

    add : nomad chapter 2

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const a = 5;
-const b = 2;
-let c = 12;
-
-const player = {
-    name: "nico",
-    sayHello: function (otherPersonsName) {
-        console.log("hello " + otherPersonsName + " nice to meet you");
+const calculator = {
+    plus: function (a, b) {
+        return (a + b);
     },
-};
+    minus: function (a, b) {
+        return (a - b);
+    },
+    times: function (a, b) {
+        return (a * b);
+    },
+    divide: function (a, b) {
+        return (a / b);
+    },
+    power: function (a, b) {
+        return (a ** b);
+    }
+}
 
-player.sayHello("lynn");
-console.log(player, console);
-player.sexy = "soon";
-console.log(player);
+const age = 23;
+function calculateKrAge(ageOfForeigner) {
+    ageOfForeigner + 2;
+}
+
+const krAge=calculateKrAge(age);
+console.log(krAge);
+
+const plusResult = calculator.plus(2,3);
+const minusResult = calculator.minus(2,3);
+const timesResult = calculator.times(2,3);
+const divideResult = calculator.divide(2,3);
+const powerResult = calculator.power(2,3);
