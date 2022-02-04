36c3f6d1c6af2e31279d81e460281ebbc393ea67
yoo-jimin127 <dbwlals9936@naver.com>
2022-02-04 21:50:09 +0900

    add : function & if-else condition

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
@@ -9,11 +10,21 @@ function onLoginSubmit(event) {
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
     localStorage.setItem(USERNAME_KEY, username);
-    greeting.innerText = `Helllo ${username}`;
+    paintGreetings(username);
+}
+
+// set greeting innerText & remove hidden css
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`; 
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+// store username at local storage
+const savedUsername = localStorage.getItem(USERNAME_KEY); 
 
-loginForm.addEventListener("submit", onLoginSubmit);
-
-const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
