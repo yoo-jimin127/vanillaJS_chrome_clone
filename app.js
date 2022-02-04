eabf17bb69984490f7cb35d15000a5a7c31cef16
yoo-jimin127 <dbwlals9936@naver.com>
2022-02-04 17:58:51 +0900

    add : user info saving by local storage

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefualt();
-    console.log(loginInput);
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    greeting.innerText = `Helllo ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event) {
-    event.preventDefualt();
-    console.log(event);
-}
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
